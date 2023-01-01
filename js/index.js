b4ec1210f8d65a02829a7071238751d093d3f95d
Randy Miller <randy918@gmail.com>
2023-01-01 13:53:50 -0600

    2nd basic commit

---
+++ js/index.js
@@ -0,0 +1,21 @@
+//!  121822.0830        VARIABLES
+
+//!  121822.0826        FUNCTIONS
+
+const init = function () {
+  main();
+};
+
+
+window.onload = function () {
+    init();
+};
+
+
+
+//!  123122.1839        MAIN
+
+const main = function () {};
+
+
+//!  123122.1839        END
